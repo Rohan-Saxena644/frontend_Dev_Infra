24296a8ab2ed5f5c04afc45c2a6a5599b917c1e0
Rohan-Saxena644 <rohsax83@gmail.com>
2026-07-15 11:07:53 +0530

    Add

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://13.206.173.255:8080/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
